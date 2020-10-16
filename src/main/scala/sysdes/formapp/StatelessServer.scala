227316ddb43b89f5296f6b33e4d85dd6670b7915
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2020-10-16 01:04:49 +0000

    download

---
+++ src/main/scala/sysdes/formapp/StatelessServer.scala
@@ -0,0 +1,29 @@
+package sysdes.formapp
+
+import java.net.Socket
+import sysdes.formapp.server.{Handler, Server}
+
+object StatelessServer extends Server(8001) {
+  override def getHandler(socket: Socket) = new StatelessServerHandler(socket)
+}
+
+class StatelessServerHandler(socket: Socket) extends Handler(socket) {
+
+  import sysdes.formapp.server.{NotFound, Ok, Request, Response}
+
+  override def handle(request: Request): Response = request match {
+    case Request("GET", "/", _, _, _) => index()
+    case _                            => NotFound(s"Requested resource '${request.path}' for ${request.method} is not found.")
+  }
+
+  def index(): Response = {
+    Ok("""<html>
+        |<body>
+        |    <form action="/form" method="post">
+        |        <input type="submit" value="start" />
+        |    </form>
+        |</body>
+        |</html>""".stripMargin)
+  }
+
+}
