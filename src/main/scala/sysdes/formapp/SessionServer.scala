173866d3c76100edf313238adf0c14c3d404950d
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2020-10-20 07:06:48 +0000

    sessionの方を一通り実装した

--- src/main/scala/sysdes/formapp/SessionServer.scala
+++ src/main/scala/sysdes/formapp/SessionServer.scala
@@ -54,6 +54,12 @@ class SessionServerHandler(socket: Socket) extends Handler(socket) {
       case Request("POST", "/form/name", _, header, _) => nameForm(header)
       case Request("POST", "/form/gender", _, header, body) =>
         genderForm(header, body)
+      case Request("POST", "/form/message", _, header, body) =>
+        messageForm(header, body)
+      case Request("POST", "/form/confirm", _, header, body) =>
+        confirm(header, body)
+      case Request("POST", "/form/finish", _, _, _) =>
+        finish()
       case _ =>
         NotFound(
           s"Requested resource '${request.path}' for ${request.method} is not found."
@@ -126,7 +132,7 @@ class SessionServerHandler(socket: Socket) extends Handler(socket) {
     }
 
     val name = params.getOrElse("name", "")
-    if (name.length() != 0) {
+    if (name != "") {
       formData.name = name
       SessionServerHandler.updateFormData(sessionID, formData)
     }
@@ -144,7 +150,115 @@ class SessionServerHandler(socket: Socket) extends Handler(socket) {
 
     val resBody = new ResponseBody(
       Array[Element](
-        new FormElement("/form/message", "post", elements.toArray)
+        new FormElement("/form/message", "post", elements.toArray),
+        new FormElement(
+          "/form/name",
+          "post",
+          Array[Element](new InputElement("submit", "", "back"))
+        )
+      )
+    )
+    Ok(resBody.toString())
+  }
+
+  def messageForm(
+      header: HashMap[String, String],
+      body: Option[String]
+  ): Response = {
+    val reqHeader            = new RequestHeader(header)
+    val reqBody: RequestBody = new RequestBody(body)
+    val params               = reqBody.parseParams()
+
+    var sessionID = ""
+    var formData  = FormData("", "", "")
+
+    getSessionIDAndFormData(reqHeader) match {
+      case (Some(_sessionID), Some(_formData)) => {
+        sessionID = _sessionID
+        formData = _formData
+      }
+      case (_, _) => return sessionIDNotFound()
+    }
+
+    val gender = params.getOrElse("gender", "")
+    if (gender != "") {
+      formData.gender = gender
+      SessionServerHandler.updateFormData(sessionID, formData)
+    }
+
+    val elements: ArrayBuffer[Element] = new ArrayBuffer[Element]()
+
+    elements.append(new TextElement("メッセージ:", true))
+    elements.append(
+      new TextAreaElement("message", formData.message, false)
+    )
+    elements.append(new TextElement("", true))
+    elements.append(new InputElement("submit", "", "next"))
+
+    val resBody = new ResponseBody(
+      Array[Element](
+        new FormElement("/form/confirm", "post", elements.toArray),
+        new FormElement(
+          "/form/gender",
+          "post",
+          Array[Element](new InputElement("submit", "", "back"))
+        )
+      )
+    )
+    Ok(resBody.toString())
+  }
+
+  def confirm(
+      header: HashMap[String, String],
+      body: Option[String]
+  ): Response = {
+    val reqHeader            = new RequestHeader(header)
+    val reqBody: RequestBody = new RequestBody(body)
+    val params               = reqBody.parseParams()
+
+    var sessionID = ""
+    var formData  = FormData("", "", "")
+
+    getSessionIDAndFormData(reqHeader) match {
+      case (Some(_sessionID), Some(_formData)) => {
+        sessionID = _sessionID
+        formData = _formData
+      }
+      case (_, _) => return sessionIDNotFound()
+    }
+
+    val message = params.getOrElse("message", "")
+    if (message != "") {
+      formData.message = message
+      SessionServerHandler.updateFormData(sessionID, formData)
+    }
+
+    val elements: ArrayBuffer[Element] = new ArrayBuffer[Element]()
+    elements.append(new TextElement(s"名前:${formData.name}", true))
+    elements.append(new TextElement(s"性別:${formData.gender}", true))
+    elements.append(new TextElement(s"メッセージ:", true))
+    elements.append(new TextAreaElement("message", formData.message, true))
+    elements.append(new TextElement("", true))
+    elements.append(new InputElement("submit", "", "submit"))
+
+    val resBody = new ResponseBody(
+      Array[Element](
+        new FormElement("/form/finish", "post", elements.toArray),
+        new FormElement(
+          "/form/message",
+          "post",
+          Array[Element](new InputElement("submit", "", "back"))
+        )
+      )
+    )
+    Ok(resBody.toString())
+  }
+
+  def finish(): Response = {
+    val resBody = new ResponseBody(
+      Array[Element](
+        new TextElement("送信しました", true),
+        new HrefElement("/", "back to top")
       )
     )
     Ok(resBody.toString())
